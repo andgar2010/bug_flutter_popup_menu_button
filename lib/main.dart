22476ce89fb6f686612a762ef7877a18cb8908eb
Andrés García <dev@tech-andgar.me>
2023-11-21 13:26:37 -0500

    feat: updated separation class simplify

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,83 +17,65 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class DropdownItem {
+  DropdownItem({required this.text, required this.icon});
 
-  final String title;
+  final IconData icon;
+  final String text;
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final formKey = GlobalKey<FormState>();
+  DropdownItem? selectedValue;
+
   @override
   Widget build(BuildContext context) {
-    final formKey = GlobalKey<FormState>();
-
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Dynamic Dropdown Demo'),
+        actions: const [DropdownSimple()],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             Padding(
-              padding: const EdgeInsets.all(24),
-              child: Expanded(
-                child: Form(
-                  key: formKey,
-                  child: Column(
-                    children: <Widget>[
-                      CustomDropdownFormField(
-                        itemBuilder: (BuildContext context) {
-                          List<DropdownItem> menuItems = List.generate(
-                            50,
-                            (index) => DropdownItem(
-                              text: 'Ítem ${index + 1}',
-                              icon: Icons.ac_unit_outlined,
-                            ),
-                          );
-
-                          return [
-                            for (DropdownItem item in menuItems)
-                              PopupMenuItem<DropdownItem>(
-                                value: item,
-                                child: Row(
-                                  children: [
-                                    Text(item.text),
-                                    const SizedBox(width: 8),
-                                    Icon(item.icon),
-                                  ],
-                                ),
-                              ),
-                          ];
-                        },
-                        validator: (DropdownItem? value) {
-                          if (value == null || value.text.isEmpty) {
-                            return 'Por favor selecciona un ítem';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(height: 8),
-                      ElevatedButton(
-                        onPressed: () {
-                          if (formKey.currentState!.validate()) {
-                            // Procesar datos del formulario
-                          }
-                        },
-                        child: const Text('Enviar'),
-                      ),
-                    ],
-                  ),
+              padding: const EdgeInsets.all(12),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    CustomDropdownFormField(
+                      onSaved: (value) => selectedValue = value,
+                      validator: (DropdownItem? value) {
+                        if (value == null || value.text.isEmpty) {
+                          return 'Por favor selecciona un ítem';
+                        }
+                        return null;
+                      },
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          // Procesar datos del formulario
+                        }
+                      },
+                      child: const Text('Submit'),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -102,51 +86,44 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class DropdownItem {
-  final String text;
-  final IconData icon;
-
-  DropdownItem({required this.text, required this.icon});
-}
-
 class CustomDropdownFormField extends FormField<DropdownItem> {
   CustomDropdownFormField({
-    super.key,
-    super.onSaved,
-    super.validator,
-    super.initialValue,
-    required List<PopupMenuEntry<DropdownItem>> Function(BuildContext)
-        itemBuilder,
-    AutovalidateMode super.autovalidateMode = AutovalidateMode.disabled,
+    Key? key,
+    FormFieldSetter<DropdownItem>? onSaved,
+    FormFieldValidator<DropdownItem>? validator,
   }) : super(
+          key: key,
+          onSaved: onSaved,
+          validator: validator,
+          initialValue: null,
           builder: (FormFieldState<DropdownItem> field) {
+            List<DropdownItem> menuItems = List.generate(
+              50,
+              (index) => DropdownItem(
+                text: 'Item ${index + 1}',
+                icon: Icons.ac_unit_outlined,
+              ),
+            );
+
             return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
+              children: [
                 PopupMenuButton<DropdownItem>(
-                  position: PopupMenuPosition.under,
-                  onSelected: (DropdownItem value) {
-                    field.didChange(value);
-                  },
-                  itemBuilder: itemBuilder,
-                  constraints: const BoxConstraints(maxHeight: 230),
-                  child: Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          field.value?.text ?? 'Selecciona un ítem',
-                          style: const TextStyle(color: Colors.black),
+                  onSelected: field.didChange,
+                  constraints: const BoxConstraints(maxHeight: 200),
+                  itemBuilder: (BuildContext context) {
+                    return menuItems.map((item) {
+                      return PopupMenuItem<DropdownItem>(
+                        value: item,
+                        child: ListTile(
+                          leading: Icon(item.icon),
+                          title: Text(item.text),
                         ),
-                        const Icon(Icons.arrow_drop_down),
-                      ],
-                    ),
+                      );
+                    }).toList();
+                  },
+                  child: ListTile(
+                    title: Text(field.value?.text ?? 'Select an item'),
+                    trailing: const Icon(Icons.arrow_drop_down),
                   ),
                 ),
                 if (field.hasError)
@@ -164,4 +141,40 @@ class CustomDropdownFormField extends FormField<DropdownItem> {
             );
           },
         );
+}
+
+class DropdownSimple extends StatelessWidget {
+  const DropdownSimple({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<DropdownItem> menuItems = List.generate(
+      50,
+      (index) => DropdownItem(
+        text: 'Item ${index + 1}',
+        icon: Icons.ac_unit_outlined,
+      ),
+    );
+
+    return PopupMenuButton<DropdownItem>(
+      onSelected: (DropdownItem value) {
+        log(value.text);
+        // Handle the selection
+      },
+      itemBuilder: (BuildContext context) {
+        return menuItems.map((DropdownItem item) {
+          return PopupMenuItem<DropdownItem>(
+            value: item,
+            child: Row(
+              children: <Widget>[
+                Icon(item.icon),
+                const SizedBox(width: 8),
+                Text(item.text),
+              ],
+            ),
+          );
+        }).toList();
+      },
+    );
+  }
 }
